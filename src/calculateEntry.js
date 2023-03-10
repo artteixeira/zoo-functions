655299a9c582f9ec123e03f93586c8e50305e06f
Arthur Teixeira <arthursony1@hotmail.com>
2023-03-10 08:20:23 -0300

    requisito 6, 7, 8 e 15

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -28,14 +28,3 @@ const calculateEntry = (entrants) => {
 };
 
 module.exports = { calculateEntry, countEntrants };
-
-const xd = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
-console.log(calculateEntry(xd));
